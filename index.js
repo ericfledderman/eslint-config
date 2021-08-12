8d17cc5fa3bdb0b827b90cb73b50834fdfeba5ad
Eric Fledderman <ericfledderman@me.com>
2021-08-12 11:39:56 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 module.exports = {
   'extends': [
+    // Aimed at promoting clean code and avoiding accidents
     './rules/best-practices',
+
+    // Points out potential runtime error in your code
     './rules/errors',
+
+    // Specifc to ECMAScript 6 development
     './rules/es6',
+
+    // Pertaining to Node.js
     './rules/node',
+
+    // Running code in strict-mode
     './rules/strict-mode',
+
+    // Promote a consistent code style accorss your project
     './rules/style',
-    './rules/variables',
-  ].map(require.resolve),
-};
+
+    // Declaration and use of variables
+    './rules/variables'
+  ].map(require.resolve)
+}
--- rules/strict-mode.js
+++ rules/strict-mode.js
@@ -1,5 +1,5 @@
 module.exports = {
   'rules': {
-    'strict': 0,
-  },
+    'strict': 0
+  }
 }
